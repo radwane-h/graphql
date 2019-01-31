852bffed46e36b4a4cf04593206641f806178433
radwane <rhassen@vente-privee.com>
2019-01-31 18:48:42 +0100

    call pim

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,33 +1,38 @@
-const graphql = require('graphql')
+const graphql = require("graphql")
+const axios = require("axios")
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
 // dummy data
-var characteristics = [
-    { code: 'SYS_24', name: 'Color' }
-]
+var characteristics = [{ code: "SYS_24", name: "Color" }]
 
 const CharacteristicType = new GraphQLObjectType({
-    name:'Characteristic',
-    fields: () => ({
-        code: { type: GraphQLString },
-        name: { type: GraphQLString }
-    })
+  name: "CharacteristicType",
+  fields: () => ({
+    code: { type: GraphQLString },
+    name: { type: GraphQLString }
+  })
 })
 
-const RootQuery = new GraphQLObjectType ({
-    name: 'RootQueryType',
-    fields: {
-        characteristic: {
-            type: CharacteristicType,
-            args: { code: { type: GraphQLString }},
-            resolve(parent, args){
-                return characteristics[0]
-            }
-        }
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    characteristic: {
+      type: CharacteristicType,
+      args: { code: { type: GraphQLString } },
+      async resolve(parent, args) {
+        return await axios
+          .get(
+            `http://pp-api-pim.vpback.vpgrp.io/v1/nomenclature/characteristic/${
+              args.code
+            }`
+          )
+          .then(resp => resp.data)
+      }
     }
+  }
 })
 
-module.exports = new GraphQLSchema ({
-    query: RootQuery
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
