a74bb99a6455c19cc9ae7d6baf87165e72dc1448
radwane <rhassen@vente-privee.com>
2019-02-01 17:44:57 +0100

    Schema change

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require("graphql")
 const axios = require("axios")
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLNonNull,
+  GraphQLList
+} = graphql
 
 // dummy data
 var characteristics = [{ code: "SYS_24", name: "Color" }]
@@ -10,7 +16,9 @@ const CharacteristicType = new GraphQLObjectType({
   name: "CharacteristicType",
   fields: () => ({
     code: { type: GraphQLString },
-    name: { type: GraphQLString }
+    name: { type: GraphQLString },
+    domainCode: { type: GraphQLString },
+    unitOfMeasurement: { type: GraphQLString }
   })
 })
 
@@ -26,6 +34,22 @@ const RootQuery = new GraphQLObjectType({
             `http://pp-api-pim.vpback.vpgrp.io/v1/nomenclature/characteristic/${
               args.code
             }`
+          )
+          .then(resp => resp.data)
+      }
+    },
+
+    characteristics: {
+      type: new GraphQLNonNull(
+        new GraphQLList(new GraphQLNonNull(CharacteristicType))
+      ),
+      args: { code: { type: GraphQLString } },
+      async resolve(parent, args) {
+        return await axios
+          .get(
+            `http://pp-api-pim.vpback.vpgrp.io/v1/nomenclature/category/${
+              args.code
+            }/characteristics`
           )
           .then(resp => resp.data)
       }
